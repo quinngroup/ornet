18755a659e11817df9fcb7ffbb029431a689c321
Marcus Hill <marcdh@uga.edu>
2020-01-21 18:17:21 -0500

    Added ffmpeg env variable via python

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,11 @@
 
+import os
 import unittest
 
 import test_pipeline
 
 if __name__ == '__main__':
+    os.environ['IMAGEIO_FFMPEG_EXE'] = 'ffmpeg'
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
     suite.addTests([
